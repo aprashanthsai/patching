286848ef2ff370c4940a9e90810e675b0fa33a1a
Prashanth Sai Athicarla <aprashanthsai@gmail.com>
2015-10-09 14:31:06 +0530

    Changed Version to 0.2.0

--- patching/metadata.rb
+++ patching/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'shivram.vedula@hpe.com'
 license 'All Rights Reserved'
 description 'Installs Vulnerability Patches'
 long_description 'Patch Bash, Samba and Java SE packages for vulnerabilities'
-version '0.1.0'
+version '0.2.0'
 
 depends 'windows'
